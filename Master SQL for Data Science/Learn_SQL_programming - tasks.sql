618ad9c5fdfc13ef89cbe7580e39e119cbfe42ff
Łukasz Bijak <105939450+LukaszBijak@users.noreply.github.com>
2022-10-30 13:57:34 +0100

    Update Learn_SQL_programming - tasks.sql

--- Master SQL for Data Science/Learn_SQL_programming - tasks.sql
+++ Master SQL for Data Science/Learn_SQL_programming - tasks.sql
@@ -1,9 +1,21 @@
-select * from people;
-select 
-	first_name,
-    last_name,
-    team,
-    quiz_points,
-    shirt_or_hat prize
-from people
-order by shirt_or_hat, team;
+
+-- Assignment 4: Practice working with data
+SELECT * FROM master_sql.people
+where first_name = 'Bonnie' and last_name = 'Brooks' ;
+
+insert into people (first_name, last_name, quiz_points, team, city, state_code, shirt_or_hat)
+	values 
+    ('Walter', 'St. John', 93, 'Baffled Badgers', 'Buffalo', 'NY', 'hat'),
+    ('Emerald', 'Chou', 92, 'Angry Ants', 'Topeka', 'KS', 'shirt');
+update people 
+	set shirt_or_hat='shirt'
+	where first_name = 'Bonnie' and last_name = 'Brooks';
+    
+select * from people
+where quiz_points = (select max(quiz_points) from people);
+
+select max(quiz_points)  from people;
+    
+
+
+	
